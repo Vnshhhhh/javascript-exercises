4594ddca8711e75255c75d55b8af0e14b6651708
vanshsubba11 <oneshvansh@gmail.com>
2026-09-01 20:21:10 +0530

    arrays-1

---
+++ script/11-arrays-and-loops.js
@@ -0,0 +1,18 @@
+const myArray =[10 ,20 ,30,];
+console.log(myArray);
+
+console.log(myArray[1]);
+myArray[0] = 99;
+console.log(myArray);
+
+[1,'vansh',true,10.5, {name :'socks'}];
+ console.log(typeof [1 ,2 ]);
+ console.log(Array.isArray([1,2]));
+
+ console.log(myArray.length);
+ myArray.push(100);
+ console.log(myArray);
+
+ myArray.splice(0 ,1);//(here 0 is index and 1 is how many elements to remove)
+ console.log(myArray);
+
