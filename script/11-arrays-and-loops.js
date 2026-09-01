74db637c53ab8b5084101879d3ad36e7f71e1702
vanshsubba11 <oneshvansh@gmail.com>
2026-09-01 23:07:52 +0530

    commit

--- script/11-arrays-and-loops.js
+++ script/11-arrays-and-loops.js
@@ -1,4 +1,4 @@
-const myArray =[10 ,20 ,30,];
+/* const myArray =[10 ,20 ,30,];
 console.log(myArray);
 
 console.log(myArray[1]);
@@ -28,3 +28,62 @@ console.log(myArray);
     {
         console.log(i);
     }
+        /*use standard loop=>for
+        non-standard loop=>while  
+
+        let randomNumber = 0;
+
+        while(randomNumber <0.5)
+            {
+                randomNumber = Math.random();
+        }
+
+        console.log(randomNumber);
+
+        
+
+        const todolist = [
+            'make dinner',//index0
+            'wash dishes',//index1
+            'do laundry',//index2
+            'clean room'//index3
+        ]
+        for (let i =0;
+             i  <= todolist.length - 1; 
+             i++)
+             {
+                const value = todolist[i];
+                console.log(value);
+            }/*looping through array  :
+        1.go through each value of an array
+        2. do soemthing with each value
+        */
+
+     
+//Accumulator pattern:
+/* 1. create a var to store the result
+2. loop thorugh the array and update th result 
+*/
+
+const nums = [1, 1, 3]
+const total=0;
+
+for(let i = 0; i < nums.length; i++)
+ //this looop will go through each value of the array
+{ const num =nums[i];
+    total += num;
+
+}
+console.log(total);
+
+// use accumulator pattern 
+const numsDoubled = [];
+for ( let i = 0; i < nums.length; i++)
+{ 
+    const num = nums[i];
+    numsDoubled.push(num * 2);
+}
+console.log(numsDoubled);
+
+
+        
